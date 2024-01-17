7139209d88f82cb97182b3602a41964ad3d661c0
Thaynison <cthaynison@outlook.com.br>
2024-01-17 10:19:52 -0300

    att

--- api/public/conexao.php
+++ api/public/conexao.php
@@ -1,8 +1,7 @@
 <?php
-
 $DB_HOST=aws.connect.psdb.cloud
-$DB_USERNAME=bihutgynq8a24pp2sy0g
-$DB_PASSWORD=pscale_pw_FSITurGdDweD9pFyaTLWQ6PAllTHypLsn3SFtPz64t4
+$DB_USERNAME=dfxmgyibyya09510v9rn
+$DB_PASSWORD=pscale_pw_bHymMmRWVCZcGLtoKb1SPY3SFMoDwdy98vEn2sbHaTx
 $DB_NAME=la-dev
 
 // Conectar ao banco de dados
