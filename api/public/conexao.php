13230d17d9b0b54f0e1b423ec566edc70b01b824
Thaynison <cthaynison@outlook.com.br>
2024-01-17 10:28:26 -0300

    att

--- api/public/conexao.php
+++ api/public/conexao.php
@@ -1,8 +1,5 @@
 <?php
-$DB_HOST=aws.connect.psdb.cloud
-$DB_USERNAME=dfxmgyibyya09510v9rn
-$DB_PASSWORD=pscale_pw_bHymMmRWVCZcGLtoKb1SPY3SFMoDwdy98vEn2sbHaTx
-$DB_NAME=la-dev
+include 'database.php';
 
 // Conectar ao banco de dados
 $conn = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
