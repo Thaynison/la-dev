b4cb47b2f9fa1e115dde01979b6a2e3b08644bd5
Thaynison <cthaynison@outlook.com.br>
2024-01-17 11:43:25 -0300

    att

--- api/index.php
+++ api/index.php
@@ -43,7 +43,7 @@
     </style>
 </head>
 <body>
-    <form action="login.php" method="post">
+    <form method="post">
         <h2>Login Form</h2>
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required>
@@ -55,3 +55,35 @@
     </form>
 </body>
 </html>
+
+<?php
+// Incluir o arquivo de conexão
+include './public/conexao.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Preparar e executar a consulta SQL
+    $stmt = $conn->prepare("SELECT * FROM usuarios WHERE username = ? AND password = ?");
+    $stmt->bind_param("ss", $username, $password);
+
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    // Verificar o resultado da consulta
+    if ($result->num_rows > 0) {
+        header("Location: ./public/ponto.php");
+        exit();
+    } else {
+        header("Location: registrar.php");
+        exit();
+    }
+
+    $stmt->close();
+}
+
+// Fechar a conexão com o banco de dados (você também pode remover esta linha se desejar manter a conexão aberta)
+$conn->close();
+?>
