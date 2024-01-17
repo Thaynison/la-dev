7900aa01dcae35bb2262cf0e09d9abe3b161644f
Thaynison <cthaynison@outlook.com.br>
2024-01-17 11:45:23 -0300

    att

--- api/index.php
+++ api/index.php
@@ -1,3 +1,32 @@
+<?php
+// Incluir o arquivo de conexão
+include './public/conexao.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Preparar e executar a consulta SQL
+    $stmt = $conn->prepare("SELECT * FROM usuarios WHERE username = ? AND password = ?");
+    $stmt->bind_param("ss", $username, $password);
+
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    // Verificar o resultado da consulta
+    if ($result->num_rows > 0) {
+        header("Location: ./public/ponto.php");
+        exit();
+    } else {
+        header("Location: registrar.php");
+        exit();
+    }
+
+    $stmt->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -55,35 +84,3 @@
     </form>
 </body>
 </html>
-
-<?php
-// Incluir o arquivo de conexão
-include './public/conexao.php';
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    // Preparar e executar a consulta SQL
-    $stmt = $conn->prepare("SELECT * FROM usuarios WHERE username = ? AND password = ?");
-    $stmt->bind_param("ss", $username, $password);
-
-    $stmt->execute();
-
-    $result = $stmt->get_result();
-
-    // Verificar o resultado da consulta
-    if ($result->num_rows > 0) {
-        header("Location: ./public/ponto.php");
-        exit();
-    } else {
-        header("Location: registrar.php");
-        exit();
-    }
-
-    $stmt->close();
-}
-
-// Fechar a conexão com o banco de dados (você também pode remover esta linha se desejar manter a conexão aberta)
-$conn->close();
-?>
